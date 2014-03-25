754b97c057927f7555c734ea01191e388001c24d
johannes-lange <johannes.lange@rwth-aachen.de>
2014-03-25 11:16:16 +0100

    find frequency

---
+++ src/FFTDevice.cpp
@@ -0,0 +1,128 @@
+#include "OutputDevice.hpp"
+
+#include <complex>
+#include <cmath>
+
+#include <QDebug>
+#include <QtEndian>
+
+namespace qTuner{
+
+OutputDevice::OutputDevice(const QAudioFormat &aFormat, QObject *parent):
+   QIODevice(parent),
+   m_audioFormat(aFormat),
+   m_iFrequencyResolution(4)
+{
+   m_iSampleBytes = m_audioFormat.sampleSize() / 8;
+}
+
+OutputDevice::~OutputDevice(){}
+
+void OutputDevice::start() { open(QIODevice::WriteOnly); }
+void OutputDevice::stop()  { close(); }
+
+qint64 OutputDevice::readData(char *data, qint64 maxlen)
+{
+   Q_UNUSED(data)
+   Q_UNUSED(maxlen)
+   return 0;
+}
+
+qint64 OutputDevice::writeData(const char *data, qint64 len)
+{
+   m_iSamples = len / m_iSampleBytes;
+
+   qint16* values = new qint16[m_iSamples];
+   const unsigned char *ptr = reinterpret_cast<const unsigned char *>(data);
+   for (int i = 0; i < m_iSamples; i++) {
+      values[i] = qFromLittleEndian<qint16>(ptr);
+      ptr += m_iSampleBytes;
+   }
+
+   double* spectrum = new double[m_iSamples];
+   fft(values, spectrum, m_iSamples);
+   //qDebug() << "raw";  dump(values, m_iSamples);
+   //qDebug() << "spec"; dump(spectrum,m_iSamples);
+   qDebug() << frequencyAt(maxPosition(spectrum, m_iSamples));
+   
+   delete[] spectrum;
+   delete[] values;
+   return len;
+}
+
+void OutputDevice::fft(qint16 data[], double spectr[], int n)
+{
+   std::complex<double>* cData = new std::complex<double>[m_iFrequencyResolution*n];
+   for (int i=0; i<n; i++)
+      cData[i] = std::complex<double>((double)data[i],0.0);
+   // add zeros for higher precision
+   for (int i=n; i<m_iFrequencyResolution*n; i++)
+      cData[i] = std::complex<double>(0.0,0.0);
+   
+   fft(cData,m_iFrequencyResolution*n);
+   for (int i=0; i<n; i++)
+      spectr[i] = std::abs<double>(cData[i]);
+
+   delete[] cData;
+}
+
+void OutputDevice::fft(std::complex<double> data[], int n)
+{
+   if (n == 1)
+      return;
+   
+   // even and uneven partition
+   std::complex<double>* ev = new std::complex<double>[n/2];
+   std::complex<double>* ue = new std::complex<double>[n/2];
+   for (int i=0; i<n/2; i++){
+      ev[i] = data[2*i];
+      ue[i] = data[2*i+1];
+   }
+   
+   fft(ev, n/2);
+   fft(ue, n/2);
+   
+   double phi;
+   for (int k = 0; k < n/2-1; k++){
+      phi = - 2.0 * M_PI * (double)k / (double)n;
+      data[k]     = ev[k] + ue[k] * std::polar(1.0, phi);
+      data[k+n/2] = ev[k] - ue[k] * std::polar(1.0, phi);
+   }
+   
+   delete[] ev;
+   delete[] ue;
+}
+
+
+void OutputDevice::dump(qint16 data[], int n)
+{
+   QString output;
+   for (int i = 0; i < n; i++) {
+      output += QString::number(data[i]) + " ";
+   }
+   qDebug() << output;
+}
+void OutputDevice::dump(double data[], int n)
+{
+   QString output;
+   for (int i = 0; i < n; i++) {
+      output += QString::number(data[i]) + " ";
+   }
+   qDebug() << output;
+}
+
+int OutputDevice::maxPosition(double data[], int n)
+{
+   int iMax = 0;
+   for (int i=0; i<n; i++){
+      if (data[i] > data[iMax])
+         iMax = i;
+   }
+   return iMax;
+}
+double OutputDevice::frequencyAt(int pos)
+{
+   return (pos + 1.0) * (double)m_audioFormat.sampleRate() / (double)m_iSamples / (double)m_iFrequencyResolution;
+}
+
+} // end namespace qTuner
